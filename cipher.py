2b8aae1d4030e8986c239ac0b40ea66c8f5de922
interestng <82784726+interestng@users.noreply.github.com>
2025-07-16 09:02:05 -0700

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,41 +1,62 @@
-num1 = float(input("Enter first number: "))
-print("Which operation?")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-operation = input("Enter operation (1, 2, 3, 4): ")
-num2 = float(input("Enter second number: "))
+cipher = {
+    'a': '@',
+    'b': '8',
+    'c': 'c',
+    'd': 'd',
+    'e': '3',
+    'f': 'f',
+    'g': 'g',
+    'h': 'h',
+    'i': '!',
+    'j': 'j',
+    'k': 'k',
+    'l': '1',
+    'm': 'm',
+    'n': 'N',
+    'o': '0',
+    'p': 'p',
+    'q': 'q',
+    'r': 'r',
+    's': '$',
+    't': '7',
+    'u': 'u',
+    'v': 'v',
+    'w': 'w',
+    'x': '%',
+    'y': 'y',
+    'z': '2'
+}
 
-if operation == "1":
-    ans = num1 + num2
-elif operation == "2":
-    ans = num1 - num2
-elif operation == "3":
-    ans = num1 * num2
-else:
-    ans = num1 / num2
+def encode(word):
+    encoded = ""
+    for letter in word:
+        encoded += cipher[letter]
+    return encoded
 
-print("Your result is", ans)
+def game():
+    print("Welcome to the game")
 
-while True:
-    choice = input("Would you like to continue? (Y/N): ")
-    if choice == "N":
-        print("bye")
-        break
+    words = ['hello', 'quick', 'smart', 'value', 'brave']
+    score = 0
+    word_number = 1
 
-    operation2 = input("Enter next operation (1, 2, 3, 4): ")
-    num3 = float(input("Enter next number: "))
+    for word in words:
+        encoded = encode(word)
+        print("Word", word_number, ":", encoded)
+        attempts = 3
+        while attempts > 0:
+            guess = input("Guess the word: ")
+            if guess == word:
+                print("Correct!")
+                score+= 1
+                break
+            else:
+                attempts -= 1
+                if attempts > 0:
+                    print("Incorrect, try again")
+                else:
+                    print("Incorrect, no more attempts. You lose")
+        word_number += 1
+    print("Game Over! Your score is ", score, "out of", len(words))
 
-    if operation2 == "1":
-        ans += num3
-    elif operation2 == "2":
-        ans -= num3
-    elif operation2 == "3":
-        ans *= num3
-    elif operation2 == "4":
-        ans /= num3
-    
-    print("Your result is", ans)
-
-        
+game()
